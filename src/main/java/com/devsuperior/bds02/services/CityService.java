9e8bfda7a4f12df4e95085bd039c32cd7e46684e
Heverton <heverton@gmail.com>
2026-01-29 08:45:00 -0300

    Finalizando Service

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,7 +1,39 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CityService {
-}
+
+    @Autowired
+    private CityRepository repository;
+
+    public List <City> findAll() {
+        return repository.findAll(Sort.by("name"));
+    }
+    public City insert(City obj) {
+        return  repository.save(obj);
+
+    }
+    public void delete(Long id) {
+        try {
+            if (!repository.existsById(id)) {
+                throw new ResourceNotFoundException("Id not found");
+            }
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+            }
+        }
+    }
+
--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,4 +1,30 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventService {
+
+    private EventRepository repository;
+
+    public Event update (Long id, Event obj) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found");
+        }
+    }
+    private void updateData (Event entity, Event obj) {
+        entity.setName(obj.getName());
+        entity.setDate(obj.getDate());
+        entity.setUrl(obj.getUrl());
+        entity.setCity(obj.getCity());
+    }
 }
